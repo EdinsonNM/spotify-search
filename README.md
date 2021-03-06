33dc4065b2d69423554c1a28b0eada8032ea8fce
Edinson Nuñez More <nmedinson@gmail.com>
2021-03-05 23:22:47 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# App Demo: Buscador spotify para microfrontend
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   rootDir: "src",
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"],
   transform: {
     "^.+\\.(j|t)sx?$": "babel-jest",
   },
   moduleNameMapper: {
-    "\\.(css)$": "identity-obj-proxy",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: [
     "../node_modules/@testing-library/jest-dom/dist/index.js",
